7051779d51efd950ef462e3fca2cea4d2ef0f12e
Gabriel Soares <gedex3d@gmail.com>
2024-12-09 00:53:03 -0300

    Adjust to work with packages

--- backend/health_test.go
+++ backend/health_test.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gabrielmenezes/financial-control/handlers"
 )
 
 func TestHealthCheck(t *testing.T){
   rr := httptest.NewRecorder()
   req := httptest.NewRequest(http.MethodGet, "/health", nil)
 
-  HealthCheck(rr, req)
+  handlers.HealthCheck(rr, req)
 
   if http.StatusOK != rr.Code {
     t.Errorf("status code expected: %d, got: %d", http.StatusOK, rr.Code)
--- backend/main.go
+++ backend/main.go
@@ -3,22 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gabrielmenezes/financial-control/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main(){
   r := mux.NewRouter()
 
-  r.HandleFunc("/health", HealthCheck).Methods("GET")
+  r.HandleFunc("/health", handlers.HealthCheck).Methods("GET")
   r.HandleFunc("/transactions", GetTransactions).Methods("GET")
 
   log.Println("Server is running on port 8080")
   log.Fatal(http.ListenAndServe(":8080", r))
-}
-
-func HealthCheck(w http.ResponseWriter, r *http.Request){
-  w.WriteHeader(http.StatusOK)
-  w.Write([]byte("API is up and running"))
 }
 
 func GetTransactions(w http.ResponseWriter, r *http.Request){
